c9866f2e845df65c5faeb9d6596f2db44545881f
Matt Mayer <mmayer@mitre.org>
2019-01-25 14:16:07 -0500

    Add exhaustive qrda round-trip and QRDA validation. Use new patient model

--- lib/qrda-import/base-importers/demographics_importer.rb
+++ lib/qrda-import/base-importers/demographics_importer.rb
@@ -6,26 +6,26 @@ module QRDA
         patient_element = patient_role_element.at_xpath('./cda:patient')
         patient.givenNames = [patient_element.at_xpath('cda:name/cda:given').text]
         patient.familyName = patient_element.at_xpath('cda:name/cda:family').text
-        patient.birthDatetime = Time.parse(patient_element.at_xpath('cda:birthTime')['value']).utc
+        patient.qdmPatient.birthDatetime = Time.parse(patient_element.at_xpath('cda:birthTime')['value']).utc
         pcbd = QDM::PatientCharacteristicBirthdate.new
-        pcbd.birthDatetime = patient.birthDatetime
+        pcbd.birthDatetime = patient.qdmPatient.birthDatetime
         pcbd.dataElementCodes = [{ code: '21112-8', codeSystem: 'LOINC' }]
-        patient.dataElements << pcbd
+        patient.qdmPatient.dataElements << pcbd
 
         pcs = QDM::PatientCharacteristicSex.new
         gender_code = patient_element.at_xpath('cda:administrativeGenderCode')['code']
         pcs.dataElementCodes = [{ code: gender_code, codeSystem: 'AdministrativeGender' }]
-        patient.dataElements << pcs
+        patient.qdmPatient.dataElements << pcs
 
         pcr = QDM::PatientCharacteristicRace.new
         race_code = patient_element.at_xpath('cda:raceCode')['code']
         pcr.dataElementCodes = [{ code: race_code, codeSystem: 'cdcrec' }]
-        patient.dataElements << pcr
+        patient.qdmPatient.dataElements << pcr
 
         pce = QDM::PatientCharacteristicEthnicity.new
         ethnicity_code = patient_element.at_xpath('cda:ethnicGroupCode')['code']
         pce.dataElementCodes = [{ code: ethnicity_code, codeSystem: 'cdcrec' }]
-        patient.dataElements << pce
+        patient.qdmPatient.dataElements << pce
 
         provider_element = doc.xpath("//cda:entry/cda:observation[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.55']")
         return if provider_element.blank?
