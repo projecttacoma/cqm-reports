e6720a328aea659088c5bb0f62fc37226d04af94
Matt Mayer <mmayer@mitre.org>
2019-02-06 14:53:54 -0500

    Fix rubocop errors

--- cqm-reports.gemspec
+++ cqm-reports.gemspec
@@ -14,8 +14,8 @@ Gem::Specification.new do |s|
   s.add_dependency 'erubis', '~> 2.7.0'
   s.add_dependency 'mongoid-tree', '~> 2.1.0'
 
-  s.add_dependency 'uuid', '~> 2.3.7'
   s.add_dependency 'nokogiri', '~> 1.8.5'
+  s.add_dependency 'uuid', '~> 2.3.7'
 
   s.add_dependency 'zip-zip', '~> 0.3'
 
--- test/unit/qrda/patient_round_trip_test.rb
+++ test/unit/qrda/patient_round_trip_test.rb
@@ -34,7 +34,6 @@ module QRDA
         doc
       end
 
-
       def test_exhaustive_patient_roundtrip
         puts "\n========================= QRDA ROUNDTRIP ========================="
         cqm_patients = QDM::PatientGeneration.generate_exhastive_data_element_patients(true)
@@ -44,44 +43,43 @@ module QRDA
           # Initial QRDA export
           begin
             exported_qrda = generate_doc(cqm_patient)
-          rescue Exception => e
-            puts "\e[#{31}mError exporting QRDA for datatype #{datatype_name}: #{e.message}\e[0m"
+          rescue StandardError => e
+            puts "\e[31mError exporting QRDA for datatype #{datatype_name}: #{e.message}\e[0m"
           end
           # Import patient from QRDA
           begin
             imported_patient = @importer.parse_cat1(exported_qrda)
-          rescue Exception => e
-            puts "\e[#{31}mError importing QRDA for datatype #{datatype_name}: #{e.message}\e[0m"
+          rescue StandardError => e
+            puts "\e[31mError importing QRDA for datatype #{datatype_name}: #{e.message}\e[0m"
           end
           # Re-export QRDA from imported patient
           begin
             # Roundtrip does not preserv extendedData
             imported_patient.qdmPatient.extendedData = cqm_patient.qdmPatient.extendedData      
             re_exported_qrda = generate_doc(imported_patient)
-          rescue Exception => e
-            puts "\e[#{31}mError Re-importing QRDA for datatype #{datatype_name}: #{e.message}\e[0m"
+          rescue StandardError => e
+            puts "\e[31mError Re-importing QRDA for datatype #{datatype_name}: #{e.message}\e[0m"
           end
 
-          if (exported_qrda && re_exported_qrda)
-            different = false
-            differences = ''
-            exported_qrda.diff(re_exported_qrda) do |change, change_info|
-              # If change represents and addition or deletion
-              if(change == "+" || change == "-")
-                # Ignore guid and generation-time based changes
-                if((!change_info.parent.to_s.include? "effectiveTime") && (!change_info.parent.to_s.include? "extension=") && (!change_info.parent.to_s.include? "root=") &&
-                  (!((change_info.parent.name.include? "time") && ((change_info.parent.parent.name.include? "legalAuthenticator") || (change_info.parent.parent.name.include? "author")))))
-                  differences += "\e[#{31}m#{change} #{change_info.to_html}\e[0m \n"
-                  different = true
-                end
+          next unless (exported_qrda && re_exported_qrda)
+          different = false
+          differences = ''
+          exported_qrda.diff(re_exported_qrda) do |change, change_info|
+            # If change represents and addition or deletion
+            if (change == "+" || change == "-")
+              # Ignore guid and generation-time based changes
+              if ((!change_info.parent.to_s.include? "effectiveTime") && (!change_info.parent.to_s.include? "extension=") && (!change_info.parent.to_s.include? "root=") &&
+                (!((change_info.parent.name.include? "time") && ((change_info.parent.parent.name.include? "legalAuthenticator") || (change_info.parent.parent.name.include? "author")))))
+                differences += "\e[31m#{change} #{change_info.to_html}\e[0m \n"
+                different = true
               end
             end
-            if (!different)
-              successful_count += 1
-            else
-              puts "\e[#{31}mERROR: Roundtrip QRDA Differences For Datatype #{datatype_name}\e[0m \n"
-              puts differences
-            end
+          end
+          if (!different)
+            successful_count += 1
+          else
+            puts "\e[31mERROR: Roundtrip QRDA Differences For Datatype #{datatype_name}\e[0m \n"
+            puts differences
           end
         end
 
@@ -95,18 +93,17 @@ module QRDA
         successful_count = 0
         cqm_patients.each do |cqm_patient|
           datatype_name = cqm_patient.givenNames[0]
-          qdm_patient = cqm_patient.qdmPatient
           begin
             exported_qrda = generate_doc(cqm_patient)
             errors = validator.validate(exported_qrda)
-            if (errors.count == 0)
+            if (errors.count.zero?)
               successful_count += 1
             end
             errors.each do |error|
-              puts "\e[#{31}mSchema Error In #{datatype_name}: #{error.message}\e[0m"
+              puts "\e[31mSchema Error In #{datatype_name}: #{error.message}\e[0m"
             end
-          rescue Exception => e
-            puts "\e[#{31}mException validating #{datatype_name}: #{e.message}\e[0m"
+          rescue StandardError => e
+            puts "\e[31mException validating #{datatype_name}: #{e.message}\e[0m"
           end
         end
         assert_equal cqm_patients.count, successful_count
