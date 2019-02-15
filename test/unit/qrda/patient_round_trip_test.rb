4ee7f78ce31c3a8584f37faa507f59eeae97871a
Matt Mayer <mmayer@mitre.org>
2019-02-15 12:44:01 -0500

    Generate measure before test

--- test/unit/qrda/patient_round_trip_test.rb
+++ test/unit/qrda/patient_round_trip_test.rb
@@ -95,15 +95,13 @@ module QRDA
         cqm_patients.each do |cqm_patient|
           datatype_name = cqm_patient.givenNames[0]
           begin
-            Timeout::timeout(5000) do
-              exported_qrda = generate_doc(cqm_patient)
-              errors = validator.validate(exported_qrda)
-              if (errors.count.zero?)
-                successful_count += 1
-              end
-              errors.each do |error|
-                puts "\e[31mSchema Error In #{datatype_name}: #{error.message}\e[0m"
-              end
+            exported_qrda = generate_doc(cqm_patient)
+            errors = validator.validate(exported_qrda)
+            if (errors.count.zero?)
+              successful_count += 1
+            end
+            errors.each do |error|
+              puts "\e[31mSchema Error In #{datatype_name}: #{error.message}\e[0m"
             end
           rescue StandardError => e
             puts "\e[31mException validating #{datatype_name}: #{e.message}\e[0m"
