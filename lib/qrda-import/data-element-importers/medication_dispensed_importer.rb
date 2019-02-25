ae6e0c5b17f358327025ed0ab54ecb33a0df8fe9
David Czulada <dczulada@mitre.org>
2019-02-25 15:33:55 -0500

    medication changes

--- lib/qrda-import/data-element-importers/medication_dispensed_importer.rb
+++ lib/qrda-import/data-element-importers/medication_dispensed_importer.rb
@@ -6,7 +6,7 @@ module QRDA
         @relevant_period_xpath = "./cda:entryRelationship/cda:supply[cda:templateId/@root = '2.16.840.1.113883.10.20.22.4.18']/cda:effectiveTime"
         @author_datetime_xpath = "./cda:entryRelationship/cda:supply[cda:templateId/@root = '2.16.840.1.113883.10.20.22.4.18']/cda:author/cda:time"
         @code_xpath = "./cda:entryRelationship/cda:supply[cda:templateId/@root = '2.16.840.1.113883.10.20.22.4.18']/cda:product/cda:manufacturedProduct/cda:manufacturedMaterial/cda:code"
-        @daysSupplied_xpath = ""
+        @days_supplied_xpath = "./cda:entryRelationship/cda:supply[cda:templateId/@root = '2.16.840.1.113883.10.20.22.4.18']/cda:entryRelationship/cda:supply[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.157']/cda:quantity"
         @prescriberId_xpath = ""
         @dispenserId_xpath = ""
         @entry_class = QDM::MedicationDispensed
@@ -14,6 +14,7 @@ module QRDA
 
       def create_entry(entry_element, nrh = NarrativeReferenceHandler.new)
         medication_dispensed = super
+        medication_dispensed.daysSupplied = extract_scalar(entry_element, @days_supplied_xpath)
         medication_dispensed
       end
 
--- lib/qrda-import/data-element-importers/medication_order_importer.rb
+++ lib/qrda-import/data-element-importers/medication_order_importer.rb
@@ -3,11 +3,13 @@ module QRDA
     class MedicationOrderImporter < MedicationImporter
       def initialize(entry_finder = QRDA::Cat1::EntryFinder.new("./cda:entry/cda:substanceAdministration[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.47']"))
         super(entry_finder)
+        @days_supplied_xpath = "./cda:entryRelationship/cda:supply[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.157']/cda:quantity"
         @entry_class = QDM::MedicationOrder
       end
 
       def create_entry(entry_element, nrh = NarrativeReferenceHandler.new)
         medication_order = super
+        medication_order.daysSupplied = extract_scalar(entry_element, @days_supplied_xpath)
         medication_order
       end
 
