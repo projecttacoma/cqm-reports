e37d992a68a958a85c219cf7a805b1ee0b1f6e62
David Czulada <dczulada@mitre.org>
2019-02-28 10:34:50 -0500

    Support QDM::PatientCharacteristicClinicalTrialParticipant

---
+++ lib/qrda-import/data-element-importers/patient_characteristic_clinical_trial_participant_importer.rb
@@ -0,0 +1,19 @@
+module QRDA
+  module Cat1
+    class PatientCharacteristicClinicalTrialParticipantImporter < SectionImporter
+      def initialize(entry_finder = QRDA::Cat1::EntryFinder.new("./cda:entry/cda:observation[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.51']"))
+        super(entry_finder)
+        @id_xpath = './cda:id'
+        @code_xpath = './cda:value'
+        @relevant_period_xpath = "./cda:effectiveTime"
+        @entry_class = QDM::PatientCharacteristicClinicalTrialParticipant
+      end
+
+      def create_entry(entry_element, nrh = NarrativeReferenceHandler.new)
+        patient_characteristic_clinical_trial_participant = super
+        patient_characteristic_clinical_trial_participant
+      end
+
+    end
+  end
+end
