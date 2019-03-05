4b42bcd5bc24ca292c15bc06b6849498fca4b895
Dave Czulada <dczulada@mitre.org>
2019-03-05 14:29:05 -0500

    Improve support for QDM::DeviceOrder

--- lib/qrda-import/data-element-importers/device_order_importer.rb
+++ lib/qrda-import/data-element-importers/device_order_importer.rb
@@ -3,7 +3,7 @@ module QRDA
     class DeviceOrderImporter < SectionImporter
       def initialize(entry_finder = QRDA::Cat1::EntryFinder.new("./cda:entry/cda:act[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.130']"))
         super(entry_finder)
-        @id_xpath = './cda:entryRelationship/cda:supply/cda:id'
+        @id_xpath = "./cda:entryRelationship/cda:supply[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.9']/cda:id"
         @code_xpath = "./cda:entryRelationship/cda:supply[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.9']/cda:participant/cda:participantRole/cda:playingDevice/cda:code"
         @author_datetime_xpath = "./cda:entryRelationship/cda:supply[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.9']/cda:author/cda:time"
         @entry_class = QDM::DeviceOrder
