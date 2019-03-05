cdf54bccf2e195d54b6e6376d4a7d4d8c502899f
Matt Mayer <mmayer@mitre.org>
2019-03-05 13:55:24 -0500

    Adverse event changes

--- lib/qrda-import/data-element-importers/adverse_event_importer.rb
+++ lib/qrda-import/data-element-importers/adverse_event_importer.rb
@@ -7,11 +7,11 @@ module QRDA
         @code_xpath = './cda:entryRelationship/cda:observation/cda:value'
         @author_datetime_xpath = './cda:author/cda:time'
         @relevant_period_xpath = './cda:effectiveTime'
-        @severity_xpath = "./cda:entryRelationship/cda:observation[cda:templateId/@root = '2.16.840.1.113883.10.20.22.4.8']/cda:value"
         @facility_locations_xpath = "./cda:participant[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.100']"
+        @severity_xpath = "./cda:entryRelationship/cda:observation[cda:templateId/@root = '2.16.840.1.113883.10.20.22.4.8']/cda:value"
+        @type_xpath = "./cda:entryRelationship/cda:observation[cda:templateId/@root = '2.16.840.1.113883.10.20.22.4.9']/cda:value"
         @entry_class = QDM::AdverseEvent
       end
-
       def create_entry(entry_element, nrh = NarrativeReferenceHandler.new)
         adverse_event = super
         adverse_event.severity = code_if_present(entry_element.at_xpath(@severity_xpath))
