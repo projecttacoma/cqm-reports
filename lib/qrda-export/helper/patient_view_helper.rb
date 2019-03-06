3feb3038bc0d4bf170c2e84a542136b6455fa5c0
Dave Czulada <dczulada@mitre.org>
2019-03-06 09:49:39 -0500

    refactor patient characteristics to be treated like other data elements

--- lib/qrda-export/helper/patient_view_helper.rb
+++ lib/qrda-export/helper/patient_view_helper.rb
@@ -31,7 +31,7 @@ module Qrda
         end
 
         def mrn
-          @qdmPatient.extendedData['medical_record_number']
+          @patient.id.to_s
         end
 
         def given_name
@@ -40,22 +40,6 @@ module Qrda
 
         def family_name
           @patient.familyName
-        end
-
-        def birthdate
-          @qdmPatient.birthDatetime.to_formatted_s(:number)
-        end
-
-        def gender
-          @qdmPatient.dataElements.where(hqmfOid: '2.16.840.1.113883.10.20.28.3.55').first.dataElementCodes.first['code']
-        end
-
-        def race
-          @qdmPatient.dataElements.where(hqmfOid: '2.16.840.1.113883.10.20.28.3.59').first.dataElementCodes.first['code']
-        end
-
-        def ethnic_group
-          @qdmPatient.dataElements.where(hqmfOid: '2.16.840.1.113883.10.20.28.3.56').first.dataElementCodes.first['code']
         end
 
         def insurance_provider
