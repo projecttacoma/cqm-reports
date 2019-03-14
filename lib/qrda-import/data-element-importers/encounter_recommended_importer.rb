db0e039243d456c4713cf9e48f294c3776b5b740
Dave Czulada <dczulada@mitre.org>
2019-03-14 15:42:45 -0400

    Use extract_facility_locations for EncounterOrder and EncounterPerformed

--- lib/qrda-import/data-element-importers/encounter_recommended_importer.rb
+++ lib/qrda-import/data-element-importers/encounter_recommended_importer.rb
@@ -12,7 +12,7 @@ module QRDA
 
       def create_entry(entry_element, nrh = NarrativeReferenceHandler.new)
         encounter_recommended = super
-        encounter_recommended.facilityLocation = extract_facility(entry_element)
+        encounter_recommended.facilityLocation = extract_facility_locations(entry_element)[0]
         encounter_recommended
       end
 
