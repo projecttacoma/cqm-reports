7d2eaf0c518740c3a8645aa8fbe226e77ac4e64c
Matt Mayer <mmayer@mitre.org>
2019-03-19 14:57:55 -0400

    Adjust frequency generation

--- lib/qrda-export/helper/frequency_helper.rb
+++ lib/qrda-export/helper/frequency_helper.rb
@@ -4,36 +4,80 @@ module Qrda
       module FrequencyHelper
         # FREQUENCY_CODE_MAP extracted from Direct Reference Codes in Opioid_v5_6_eCQM.xml (CMS460v0)
         FREQUENCY_CODE_MAP = {
-          '229799001' => { in_hours: 12, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', codeSystemVersion: '2018-03', displayName: 'Twice a day (qualifier value)' },
-          '307470009' => { in_hours: 12, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', codeSystemVersion: '2018-03', displayName: 'Every twelve hours (qualifier value)' },
-          '396131002' => { in_hours: 48, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', codeSystemVersion: '2018-03', displayName: 'Every forty eight hours (qualifier value)' },
-          '396125000' => { in_hours: 24, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', codeSystemVersion: '2018-03', displayName: 'Every twenty four hours (qualifier value)' },
-          '396109005' => { in_hours: 6, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', displayName: 'One to four times a day (qualifier value)' },
-          '396126004' => { in_hours: 36, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', codeSystemVersion: '2018-03', displayName: 'Every thirty six hours (qualifier value)' },
-          '225756002' => { in_hours: 4, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', displayName: 'Every four hours (qualifier value)' },
-          '396139000' => { in_hours: 6, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', displayName: 'Every six to eight hours (qualifier value)' },
-          '225754004' => { in_hours: 3, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', displayName: 'Every three to four hours (qualifier value)' },
-          '396140003' => { in_hours: 8, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', displayName: 'Every eight to twelve hours (qualifier value)' },
-          '396107007' => { in_hours: 12, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', displayName: 'One to two times a day (qualifier value)' },
-          '396108002' => { in_hours: 8, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', displayName: 'One to three times a day (qualifier value)' },
-          '396111001' => { in_hours: 12, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', displayName: 'Two to four times a day (qualifier value)' },
-          '307469008' => { in_hours: 8, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', codeSystemVersion: '2018-03', displayName: 'Every eight hours (qualifier value)' },
-          '229797004' => { in_hours: 24, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', codeSystemVersion: '2018-03', displayName: 'Once daily (qualifier value)' },
-          '229798009' => { in_hours: 8, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', codeSystemVersion: '2018-03', displayName: 'Three times daily (qualifier value)' },
-          '307439001' => { in_hours: 6, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', codeSystemVersion: '2018-03', displayName: 'Four times daily (qualifier value)' },
-          '307468000' => { in_hours: 6, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', codeSystemVersion: '2018-03', displayName: 'Every six hours (qualifier value)' },
-          '396127008' => { in_hours: 3, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', displayName: 'Every three to six hours (qualifier value)' },
-          '225752000' => { in_hours: 2, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', displayName: 'Every two to four hours (qualifier value)' },
-          '396143001' => { in_hours: 72, codeSystem: '2.16.840.1.113883.6.96', codeSystemName: 'SNOMEDCT', codeSystemVersion: '2018-03', displayName: 'Every seventy two hours (qualifier value)' }
+          '396107007' => { low: 12, high: 24, unit: 'h', institution_specified: true, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', display_name: 'One to two times a day (qualifier value)' },
+          '396108002' => { low: 8, high: 24, unit: 'h', institution_specified: true, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', display_name: 'One to three times a day (qualifier value)' },
+          '396109005' => { low: 6, high: 24, unit: 'h', institution_specified: true, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', display_name: 'One to four times a day (qualifier value)' },
+          '396111001' => { low: 6, high: 12, unit: 'h', institution_specified: true, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', display_name: 'Two to four times a day (qualifier value)' },
+
+          '229797004' => { low: 24, high: nil, unit: 'h', institution_specified: true, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', code_system_version: '2018-03', display_name: 'Once daily (qualifier value)' },
+          '229799001' => { low: 12, high: nil, unit: 'h', institution_specified: true, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', code_system_version: '2018-03', display_name: 'Twice a day (qualifier value)' },
+          '229798009' => { low: 8, high: nil, unit: 'h', institution_specified: true, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', code_system_version: '2018-03', display_name: 'Three times daily (qualifier value)' },
+          '307439001' => { low: 6, high: nil, unit: 'h', institution_specified: true, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', code_system_version: '2018-03', display_name: 'Four times daily (qualifier value)' },
+
+          '225752000' => { low: 2, high: 4, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', display_name: 'Every two to four hours (qualifier value)' },
+          '225754004' => { low: 3, high: 4, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', display_name: 'Every three to four hours (qualifier value)' },
+          '396127008' => { low: 3, high: 6, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', display_name: 'Every three to six hours (qualifier value)' },
+          '396139000' => { low: 6, high: 8, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', display_name: 'Every six to eight hours (qualifier value)' },
+          '396140003' => { low: 8, high: 12, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', display_name: 'Every eight to twelve hours (qualifier value)' },
+
+          '225756002' => { low: 4, high: nil, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', display_name: 'Every four hours (qualifier value)' },
+          '307468000' => { low: 6, high: nil, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', code_system_version: '2018-03', display_name: 'Every six hours (qualifier value)' },
+          '307469008' => { low: 8, high: nil, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', code_system_version: '2018-03', display_name: 'Every eight hours (qualifier value)' },
+          '307470009' => { low: 12, high: nil, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', code_system_version: '2018-03', display_name: 'Every twelve hours (qualifier value)' },
+          '396125000' => { low: 24, high: nil, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', code_system_version: '2018-03', display_name: 'Every twenty four hours (qualifier value)' },
+          '396126004' => { low: 36, high: nil, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', code_system_version: '2018-03', display_name: 'Every thirty six hours (qualifier value)' },
+          '396131002' => { low: 48, high: nil, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', code_system_version: '2018-03', display_name: 'Every forty eight hours (qualifier value)' },
+          '396143001' => { low: 72, high: nil, unit: 'h', institution_specified: false, code_system: '2.16.840.1.113883.6.96', code_system_name: 'SNOMEDCT', code_system_version: '2018-03', display_name: 'Every seventy two hours (qualifier value)' }
         }.freeze
 
         def medication_frequency
-          # If the code matches one of the known Direct Reference Codes, export that time in hours. Otherwise default to 24 hours
-          medication_period = FREQUENCY_CODE_MAP[self['code']]&.in_hours || 24
-          "<effectiveTime xsi:type='PIVL_TS' institutionSpecified='true' operator='A'>"\
-          "<period value='#{medication_period}' unit='h'/>"\
+          # If the code matches one of the known Direct Reference Codes, export that time in hours. Otherwise default to "every twenty four hours" code
+          frequency_code_entry = FREQUENCY_CODE_MAP[self['code']] || FREQUENCY_CODE_MAP['396125000']
+          if(!frequency_code_entry[:institution_specified])
+            if(frequency_code_entry[:high].nil?)
+              institution_not_specified_point_frequency(frequency_code_entry)
+            else
+              institution_not_specified_range_frequency(frequency_code_entry)
+            end
+          else
+            if(frequency_code_entry[:high].nil?)
+              institution_specified_point_frequency(frequency_code_entry)
+            else
+              institution_specified_range_frequency(frequency_code_entry)
+            end
+          end
+        end
+
+        def institution_not_specified_point_frequency(frequency_code_entry)
+          "<effectiveTime xsi:type='PIVL_TS' operator='A'>"\
+          "<period value='#{frequency_code_entry[:low]}' unit='#{frequency_code_entry[:unit]}'/>"\
           "</effectiveTime>"
         end
+
+        def institution_not_specified_range_frequency(frequency_code_entry)
+          "<effectiveTime xsi:type='PIVL_TS' operator='A'>"\
+          "<period xsi:type='IVL_PQ'>"\
+          "<low value='#{frequency_code_entry[:low]}' unit='#{frequency_code_entry[:unit]}'/>"\
+          "<high value='#{frequency_code_entry[:high]}' unit='#{frequency_code_entry[:unit]}'/>"\
+          "</period>"
+          "</effectiveTime>"
+        end
+
+        def institution_specified_point_frequency(frequency_code_entry)
+          "<effectiveTime xsi:type='PIVL_TS' institutionSpecified='true' operator='A'>"\
+          "<period value='#{frequency_code_entry[:low]}' unit='#{frequency_code_entry[:unit]}'/>"\
+          "</effectiveTime>"
+        end
+
+        def institution_specified_range_frequency(frequency_code_entry)
+          "<effectiveTime xsi:type='PIVL_TS' institutionSpecified='true' operator='A'>"\
+          "<period xsi:type='IVL_PQ'>"\
+          "<low value='#{frequency_code_entry[:low]}' unit='#{frequency_code_entry[:unit]}'/>"\
+          "<high value='#{frequency_code_entry[:high]}' unit='#{frequency_code_entry[:unit]}'/>"\
+          "</period>"
+          "</effectiveTime>"
+        end
+
       end
     end
   end
