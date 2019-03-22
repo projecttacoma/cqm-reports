b38a12657d74b6a778b06949e34f1bb2926ddf40
Matt Mayer <mmayer@mitre.org>
2019-03-22 11:26:44 -0400

    Fix final rubocop error

--- lib/qrda-export/helper/cat1_view_helper.rb
+++ lib/qrda-export/helper/cat1_view_helper.rb
@@ -69,7 +69,7 @@ module Qrda
           return "<value xsi:type=\"PQ\" value=\"#{result['value']}\" unit=\"#{result['unit']}\"/>" if result['unit']
         end
 
-        def authorDateTime_or_dispenserId?
+        def authordatetime_or_dispenserid?
           self['authorDatetime'] || self['dispenserId']
         end
       end
