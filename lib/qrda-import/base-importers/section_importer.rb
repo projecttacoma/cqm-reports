35f6d282c88af82c3e7a25d3b899d109eb376102
Matt Mayer <mmayer@mitre.org>
2019-06-13 11:41:07 -0400

    Appease rubocop

--- lib/qrda-import/base-importers/section_importer.rb
+++ lib/qrda-import/base-importers/section_importer.rb
@@ -125,7 +125,7 @@ module QRDA
         # If a Direct Reference Code isn't found, return nil
         return nil unless key
         # If a Direct Reference Code is found, return that code
-        oid =value[:codeSystem] || value[:codeSystemOid]
+        oid = value[:codeSystem] || value[:codeSystemOid]
         QDM::Code.new(key, oid)
       end
 
