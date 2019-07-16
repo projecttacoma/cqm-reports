7827420613adcd35bd615841cb3e7b1fe238ef9a
Dave Czulada <dczulada@mitre.org>
2019-07-16 10:42:37 -0400

    fixed some issues from c1 record sample roundtrip

--- lib/qrda-import/data-element-importers/patient_characteristic_clinical_trial_participant_importer.rb
+++ lib/qrda-import/data-element-importers/patient_characteristic_clinical_trial_participant_importer.rb
@@ -6,11 +6,13 @@ module QRDA
         @id_xpath = './cda:id'
         @code_xpath = './cda:value'
         @relevant_period_xpath = "./cda:effectiveTime"
+        @reason_xpath = "./cda:entryRelationship[@typeCode='RSON']/cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.24.3.88']/cda:value"
         @entry_class = QDM::PatientCharacteristicClinicalTrialParticipant
       end
 
       def create_entry(entry_element, nrh = NarrativeReferenceHandler.new)
         patient_characteristic_clinical_trial_participant = super
+        patient_characteristic_clinical_trial_participant.reason = extract_reason(entry_element)
         patient_characteristic_clinical_trial_participant
       end
 
